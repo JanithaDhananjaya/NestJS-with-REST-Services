904600fffeff346fb94d3a5df9216bf8d9b5a78b
janithasilva <ajdsilva@zuelligpharma.com>
2024-02-14 17:46:36 +0530

    employee get and post APIs created!

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,11 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
+import { EmployeeTier } from "./employee.model";
 
 @Controller('employees')
 export class EmployeesController {
   constructor(private employeeService: EmployeesService) {}
+
   @Get()
   getAllEmployees() {
     return this.employeeService.getAllEmployees();
+  }
+
+  @Post()
+  createEmployee(
+    @Body('firstName') firstName: string,
+    @Body('lastName') lastName: string,
+    @Body('designation') designation: string,
+    @Body('nearestCity') nearestCity: string,
+    @Body('tier') tier: number,
+  ) {
+    return this.employeeService.createEmployee(
+      firstName,
+      lastName,
+      designation,
+      nearestCity,
+      tier,
+    );
   }
 }
--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -1,10 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { Employee, EmployeeTier } from './employee.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class EmployeesService {
-  private employees = [];
+  private employees: Employee[] = [];
 
   getAllEmployees() {
     return this.employees;
+  }
+
+  createEmployee(
+    firstName: string,
+    lastName: string,
+    designation: string,
+    nearestCity: string,
+    tier: EmployeeTier,
+  ) {
+    const employee = {
+      id: uuid(),
+      firstName,
+      lastName,
+      designation,
+      nearestCity,
+      tier,
+    };
+
+    this.employees.push(employee);
+    return employee;
   }
 }
