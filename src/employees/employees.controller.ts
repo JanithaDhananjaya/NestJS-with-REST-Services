832789eafb94146fda9d9dcc38793526c992f304
janithasilva <ajdsilva@zuelligpharma.com>
2024-02-17 15:03:21 +0530

    employee crud update/search/delete

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,30 +1,57 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { EmployeesService } from './employees.service';
-import { EmployeeTier } from "./employee.model";
+import { Body, Controller, Delete, Get, HttpCode, NotFoundException, Param, Post, Put, Query } from "@nestjs/common";
+import { EmployeesService } from "./employees.service";
+import { Employee, EmployeeTier } from "./Employee.model";
+import { EmployeeSearchDto } from "./EmployeeSearch.dto";
+import { EmployeeUpdateDto } from "./EmployeeUpdate.dto";
 
-@Controller('employees')
+@Controller("employees")
 export class EmployeesController {
-  constructor(private employeeService: EmployeesService) {}
+  constructor(private employeeService: EmployeesService) {
+  }
 
   @Get()
-  getAllEmployees() {
-    return this.employeeService.getAllEmployees();
+  getAllEmployees(@Query() param: EmployeeSearchDto) {
+    if (Object.keys(param).length) {
+      return this.employeeService.employeeSearch(param);
+    } else {
+      return this.employeeService.getAllEmployees();
+    }
   }
 
   @Post()
   createEmployee(
-    @Body('firstName') firstName: string,
-    @Body('lastName') lastName: string,
-    @Body('designation') designation: string,
-    @Body('nearestCity') nearestCity: string,
-    @Body('tier') tier: number,
+    @Body("firstName") firstName: string,
+    @Body("lastName") lastName: string,
+    @Body("designation") designation: string,
+    @Body("nearestCity") nearestCity: string,
+    @Body("tier") tier: EmployeeTier
   ) {
     return this.employeeService.createEmployee(
       firstName,
       lastName,
       designation,
       nearestCity,
-      tier,
+      tier
     );
   }
+
+  @Get("/:id")
+  getEmployeeById(@Param("id") id: string) {
+    return this.employeeService.getEmployeeById(id);
+  }
+
+  @Put("/:id/city")
+  updateEmployee(@Param("id") id: string, @Body() employeeUpdateDto: EmployeeUpdateDto) {
+    employeeUpdateDto.id = id;
+    return this.employeeService.updateEmployee(employeeUpdateDto);
+  }
+
+  @Delete("/:id")
+  @HttpCode(204)
+  deleteEmployee(@Param("id") id: string) {
+    if (!this.employeeService.deleteEmployee(id)) {
+      throw new NotFoundException("Employee does not exist");
+    }
+  }
+
 }
--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
-import { Employee, EmployeeTier } from './employee.model';
-import { v1 as uuid } from 'uuid';
+import { Injectable } from "@nestjs/common";
+import { Employee, EmployeeStatus, EmployeeTier } from "./Employee.model";
+import { v1 as uuid } from "uuid";
+import { EmployeeSearchDto } from "./EmployeeSearch.dto";
+import { EmployeeUpdateDto } from "./EmployeeUpdate.dto";
 
 @Injectable()
 export class EmployeesService {
@@ -15,7 +17,7 @@ export class EmployeesService {
     lastName: string,
     designation: string,
     nearestCity: string,
-    tier: EmployeeTier,
+    tier: EmployeeTier
   ) {
     const employee = {
       id: uuid(),
@@ -24,9 +26,46 @@ export class EmployeesService {
       designation,
       nearestCity,
       tier,
+      status: EmployeeStatus.ACTIVE
     };
 
     this.employees.push(employee);
     return employee;
+  }
+
+  employeeSearch(employeeSearchDto: EmployeeSearchDto) {
+    const { status, name } = employeeSearchDto;
+    let employees = this.getAllEmployees();
+    if (status) {
+      employees = employees.filter((employee) => employee.status === status);
+    }
+    if (name) {
+      employees = employees.filter(
+        (employee) =>
+          employee.firstName.includes(name) || employee.lastName.includes(name)
+      );
+    }
+
+    return employees;
+  }
+
+  getEmployeeById(id: string) {
+    const employees = this.getAllEmployees();
+    return employees.find(employee => employee.id === id);
+  }
+
+  updateEmployee(employeeUpdateDto: EmployeeUpdateDto): Employee {
+    const { id, city } = employeeUpdateDto;
+
+    let employeeById = this.getEmployeeById(id);
+    employeeById.nearestCity = city;
+    return employeeById;
+
+  }
+
+  deleteEmployee(id: string) : boolean {
+    let employees = this.getAllEmployees();
+    this.employees = employees.filter(employee => employee.id !== id);
+    return (employees.length != this.employees.length);
   }
 }
